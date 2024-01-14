042bf03ad2230eb56a7dfb66b814a7a8b82e7f79
tuhin201765 <tuhin201765@gmail.com>
2024-01-14 17:08:18 +0600

    if

--- bot.py
+++ bot.py
@@ -8,36 +8,58 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import os
 import csv
+import datetime
+def scrape_and_write_to_csv():
+    driver = Chrome()
+    driver.get("http://www.eventkeeper.com/mars/xpages/B/BETHEL/EK.cfm?zeeOrg=BETHEL")
+    sleep(5)
+    driver.refresh()
+    wait = WebDriverWait(driver, 20) 
+    all_dates = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='event_date_row']")))
+    wait = WebDriverWait(driver, 20)  
+    all_events = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='one_event']")))
+    with open('event_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(['eventTitle', 'eventRegisterLink', 'eventOrganizer','eventOrganizerPhone', 'start_datetime_formatted', 'end_datetime_formatted', 'start_time', 'end_time', 'eventOrganizerEmail', 'eventImageURL', 'eventDescription', 'eventVenueName'])
 
-driver = Chrome()
-driver.get("http://www.eventkeeper.com/mars/xpages/B/BETHEL/EK.cfm?zeeOrg=BETHEL")
-sleep(5)
-driver.refresh()
-wait = WebDriverWait(driver, 20) 
-all_dates = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='event_date_row']")))
-wait = WebDriverWait(driver, 20)  
-all_events = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//div[@class='one_event']")))
-with open('event_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['Name', 'Date', 'Mail', 'Image URL', 'Description', 'Location'])
+        for event in all_events:
+            eventTitle = event.find_element("xpath",".//div[@class='event_name']").text
 
-    for event in all_events:
-        name = event.find_element("xpath",".//div[@class='event_name']").text
-        date_element = event.find_element("xpath",".//div[@class='event_time']")
-        date = date_element.text
-        mail = event.find_element("xpath",".//div[@class='event_contact']/a")
-        image_element = event.find_element("xpath", ".//div[@class='event_description']/p[1]/img")
-        image_url = image_element.get_attribute("src")
+            wait = WebDriverWait(driver, 10)
+            input_element = wait.until(EC.presence_of_element_located((By.XPATH, "//div[@class='event_registration']/input")))
+            eventRegister = input_element.get_attribute("onclick")
+            link_start = eventRegister.find("'") + 1
+            link_end = eventRegister.rfind("'")
+            eventRegisterLink = eventRegister[link_start:link_end]
 
-        description = event.find_elements("xpath",".//div[@class='event_description']/p/span")
-        if description:
-            description = description[0].text
-        # contact = event.find_element("xpath","//div[@class='event_contact']/text()[1]")
-        location = event.find_element("xpath",".//div[@class='event_location']")
-        if location:
-            location = location.text
-        else:
-            pass
-        csvwriter.writerow([name, date, mail, image_url, description, location])
+            time_element = event.find_element("xpath",".//div[@class='event_time']")
+            time = time_element.text
+            start_time, end_time = map(str.strip, time.split('-'))
+
+            contact_element = event.find_element("xpath", "//div[@class='event_contact']")
+            contact_text = contact_element.text
+            contact_info = contact_text.split(':')
+            eventOrganizer = contact_info[1].split('(')[0].strip()
+
+            eventOrganizerPhone = ''.join(char for char in contact_info[1].split('(')[1] if char.isdigit())
+
+            eventOrganizerEmail = event.find_element("xpath",".//div[@class='event_contact']/a")
+
+            image_element = event.find_element("xpath", ".//div[@class='event_description']/p[1]/img")
+            eventImageURL = image_element.get_attribute("src")
+
+            description = event.find_elements("xpath",".//div[@class='event_description']/p/span")
+            eventDescription = description[0].text if description else None  
+
+            location = event.find_element("xpath",".//div[@class='event_location']")
+            eventVenueName = location.text if location else None
+
+            csvwriter.writerow([eventTitle, eventRegisterLink, end_time, start_time, eventOrganizerEmail, eventOrganizer, eventOrganizerPhone, eventImageURL, eventDescription, eventVenueName])
+    driver.quit()
 
 
+while True:
+    scrape_and_write_to_csv()
+    sleep(600)
+
+     # contact = event.find_element("xpath","//div[@class='event_contact']/text()[1]")
